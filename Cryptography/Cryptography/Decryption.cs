309e32f7a791eaca1bf8685ac411a39fd12c5dcd
skeat59 <gdalia_t@epita.fr>
2013-11-19 13:02:33 +0100

    first commit

---
+++ Cryptography/Cryptography/Decryption.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Crypto;
+
+namespace Cryptography
+{
+    static class Decryption
+    {
+        public static uint Decalcb(char original, char encrypted)
+        {
+            uint res = 0;
+            if (original < encrypted)
+            {
+                res = 26 - (uint)(encrypted - original);
+            }
+            else
+            {
+                res = (uint)(encrypted - original);
+            }
+            return res;
+        }
+
+        public static char Decrypted(char encryp, uint n)
+        {
+            char res = Convert.ToChar((encryp - 'a' + n)%26 + 'a');
+            return res;
+        }
+    }
+}
---
+++ Cryptography/Cryptography/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Forms;
+using Crypto;
+
+namespace Cryptography
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Crypto.Function.DecryptRot = Decryption.Decalcb ;
+            Crypto.Function.DecryptionFunc = Decryption.Decrypted;
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Decrypt());
+        }
+    }
+}
